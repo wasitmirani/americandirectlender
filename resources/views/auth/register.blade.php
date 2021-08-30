cfc427b337d92065650c3eca94662e07585fc017
Hassoku <mhhassan095@gmail.com>
2021-08-30 15:07:47 -0700

    third commmit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,7 +48,7 @@
                                         <div class="input-group-text">
                                             <i class="fe fe-mail"></i>
                                         </div>
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    <input id="email" type="email" class="form-control @error('name') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
