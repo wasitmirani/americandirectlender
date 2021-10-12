8fd5909b392869f5342e08ba2d2cbcfab3ed1daf
HaseebHassan <mhhassan095@gmail.com>
2021-10-12 16:15:15 -0700

    notification component added

---
+++ resources/js/components/backend/pages/content/notification/ShowNotification.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <p>Notifications</p>
+    </div>
+</template>
+<script>
+</script>
--- resources/views/layouts/backend/components/sidebar.blade.php
+++ resources/views/layouts/backend/components/sidebar.blade.php
@@ -3,7 +3,7 @@
         <img class="img-90 rounded-circle" src="../assets/images/dashboard/1.png" alt="">
 
         <h6 class="mt-3 f-14 f-w-600">{{ Auth::user()->name }}</h6>
-      <p class="mb-0 font-roboto">{{ Auth::user()->roles[0] ? Auth::user()->roles[0]->name  : "Customer" }}</p>
+      <p class="mb-0 font-roboto"></p>
 
       </ul>
     </div>
