014542161761c5097c72bb999ee2334e7e429f3f
HaseebHassan <mhhassan095@gmail.com>
2021-10-12 17:37:39 -0700

    email template modified

--- resources/views/emails/contactmail.blade.php
+++ resources/views/emails/contactmail.blade.php
@@ -4,9 +4,8 @@
      </head>
      <body>
          <p>Contaact Message</p>
-         {{ $data['name'] }}
-
-{{ $data['email'] }}
-{{ $data['message'] }}
+         <h1>{{ $data['name'] }}</h1>
+         <h1>{{ $data['email'] }}</h1>
+        <h1> {{ $data['message'] }}</h1>
      </body>
  </html>
