7f4b46587a050ec2e6da4ff61d0ec948c8067b7d
HaseebHassan <mhhassan095@gmail.com>
2021-10-29 12:20:37 -0700

    chart added

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -25,7 +25,7 @@ class DashboardController extends Controller
         $agents=Role::select('name')->withCount('users')->get();
 
 
-                       $applications= DB::table('applications')->select(DB::raw('DATE(created_at) as created_at'), DB::raw('COUNT(*) as applications'))->
+         $applications= DB::table('applications')->select(DB::raw('DATE(created_at) as created_at'), DB::raw('COUNT(*) as total'))->
                        groupBy(DB::raw('DATE(created_at)'))->
                        get();
 
